87796fce9bd41ab06cb6f45ef483a76ce4b2ade1
Shawn Anderson <shawn42@gmail.com>
2009-03-20 08:16:08 -0700

    changed obj removal ordering

--- lib/gamebox/physical_director.rb
+++ lib/gamebox/physical_director.rb
@@ -4,7 +4,7 @@ class PhysicalDirector < Director
   def remove_physical_obj(shape)
     obj_to_kill = @actors.select{|a|a.shape==shape}.first
     obj_to_kill.remove_self
-    obj_to_kill.level.unregister_physical_object obj_to_kill
     @actors.delete obj_to_kill
+    obj_to_kill.level.unregister_physical_object obj_to_kill
   end
 end
