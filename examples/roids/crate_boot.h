dc2c1d08bfbb22d1cb01c9b531625358c94b548d
Shawn Anderson <shawn42@gmail.com>
2009-03-29 18:11:05 -0700

    gem cleanup

--- examples/roids/crate_boot.h
+++
@@ -1,8 +0,0 @@
-/**
- * Automatcially generated.  Do not edit.  To change the contents of
- * this file, update your project main_file, main_class and run_method
- * options and rebuild.
- */
-#define CRATE_MAIN_FILE  "app"
-#define CRATE_MAIN_CLASS "GameboxApp"
-#define CRATE_RUN_METHOD "run"
