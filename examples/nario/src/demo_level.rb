6346f02d5a59ae22a38b3e62946e4ba36222414f
Shawn Anderson <shawn42@gmail.com>
2009-04-10 08:50:45 -0700

    fixed static shapes to have Inf mass/moment and no longer add the bodies to the space; added static powerup blocks to nario

--- examples/nario/src/demo_level.rb
+++ examples/nario/src/demo_level.rb
@@ -7,18 +7,30 @@ class DemoLevel < PhysicalLevel
     @space.iterations = 3
     @space.damping = 0.7
 
-    @bg = create_actor :nario_background
+    bg = create_actor :nario_background
 
-    @ground = create_actor :ground
-    @ground.warp vec2(0,700)
-    @ground.shape.e = 0.0004
-    @ground.shape.u = 0.0004
+    ground = create_actor :ground
+    ground.warp vec2(0,700)
+    ground.shape.e = 0.0004
+    ground.shape.u = 0.0004
 
-    @nario = create_actor :nario
-    @nario.warp vec2(300,500)
+
+    # TODO this is a hack
+    require 'animated_actor_view'
+    pup = create_actor :power_up_block, :view => AnimatedActorView
+    pup.body.a -= 1.57
+    pup.warp vec2(500,400)
+
+    nario = create_actor :nario
+    nario.warp vec2(300,500)
 
     # stand nario upright
-    @nario.body.a -= 1.57
+    nario.body.a -= 1.57
+
+    @space.add_collision_func(:power_up_block, :ground) do |pup,g|
+      p "power_up_block hit the ground"
+    end
+
   end
 
   def draw(target)
--- examples/roids/src/walls.rb
+++ examples/roids/src/walls.rb
@@ -3,27 +3,23 @@ require 'actor'
 class LeftWall < Actor
   has_behaviors :physical => {:shape => :poly, 
     :fixed => true,
-    :mass => 100,
     :verts => [[0,0],[0,800],[1,800],[1,0]]}
 end
 
 class TopWall < Actor
   has_behaviors :physical => {:shape => :poly, 
     :fixed => true,
-    :mass => 100,
     :verts => [[0,0],[0,1],[1024,1],[1024,0]]}
 end
 
 class BottomWall < Actor
   has_behaviors :physical => {:shape => :poly, 
     :fixed => true,
-    :mass => 100,
     :verts => [[0,0],[0,1],[1024,1],[1024,0]]}
 end
 
 class RightWall < Actor
   has_behaviors :physical => {:shape => :poly, 
     :fixed => true,
-    :mass => 100,
     :verts => [[0,0],[0,800],[1,800],[1,0]]}
 end
