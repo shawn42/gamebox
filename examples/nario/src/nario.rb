1c5c35337aa68746155ec626a4993dfd157a5861
Shawn Anderson <shawn42@gmail.com>
2009-04-10 09:00:31 -0700

    chipmunk API fix for mass not found

--- examples/nario/src/nario.rb
+++ examples/nario/src/nario.rb
@@ -6,7 +6,8 @@ class Nario < Actor
     :verts => [[-17,-20],[-17,20],[17,20],[17,-20]]}
 
   def setup
-    mass = self.body.mass
+    # HACK because some people's chipmunk is old?
+    mass = 100 #self.body.mass
     @speed = mass * 0.02
     @jump_speed = -300*@speed
     @max_speed = 100
