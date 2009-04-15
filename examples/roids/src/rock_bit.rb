8cffe1d7c939aa5eb5019f88238f7066c5be1b4f
Shawn Anderson <shawn42@gmail.com>
2009-04-14 17:17:47 -0700

    fixed roid speed

--- examples/roids/src/rock_bit.rb
+++ examples/roids/src/rock_bit.rb
@@ -10,7 +10,9 @@ class RockBit < Actor
     @behaviors[:physical].body.a -= rand(10)
     @speed = (rand(2)+1)/6.0
     @turn_speed = rand(2)*0.00004
-    @dir = vec2(rand-0.5,rand-0.5)
+    x = (rand-0.5) * 2
+    y = (rand-0.5) * 2
+    @dir = vec2(x,y)
 
     #bits only live for 500-1000
     @ttl = 500+rand(500)
