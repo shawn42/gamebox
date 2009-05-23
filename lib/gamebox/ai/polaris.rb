27b8ac3f96195612a4209c6174033cf6b49e10b8
Shawn Anderson <shawn42@gmail.com>
2009-05-23 10:04:55 -0700

    added polaris pathfinder and spec;needs performance tuning

---
+++ lib/gamebox/ai/polaris.rb
@@ -0,0 +1,88 @@
+
+# Polaris is the star that guides, aka "The North Star".  It implements the A* algorithm.
+class Polaris
+  
+  def initialize(map)
+    @map = map
+  end
+  
+  def guide(from, to, max_depth=400)
+    return nil if @map.blocked? from or @map.blocked? to
+    from_element = PathElement.new(from)
+    from_element.dist_from = @map.distance(from,to)
+    open = [from_element]
+    closed = []
+    step = 0
+    
+    until open.empty? or step > max_depth
+      step += 1
+      
+      open = open.sort_by{|n|n.rating}
+      
+      current_element = open.shift
+      loc = current_element.location
+      if @map.cost(loc,to) == 0
+        path = []
+        until current_element.parent.nil?
+          path.unshift current_element
+          current_element = current_element.parent
+        end
+
+        return path
+      else
+        closed << current_element
+        
+        for next_door in @map.neighbors(loc)
+          next unless closed.select{|c|c.location.x == next_door.x and c.location.y == next_door.y}.empty?
+          
+          unless @map.blocked? next_door
+            next_door_element = open.select{|o|o.location.x == next_door.x and o.location.y == next_door.y}.first
+            g = current_element.cost_to + @map.cost(loc, next_door)
+            if next_door_element.nil?
+              # add to open
+              el = PathElement.new(next_door,current_element)
+              el.cost_to = g
+              el.dist_from = @map.distance(next_door,to)
+              open << el
+            elsif next_door_element.cost_to > g
+              # update the parent and cost
+              next_door_element.parent = current_element
+              next_door_element.cost_to = g
+            end
+          end
+        end
+      end
+    end
+    nil
+  end
+end
+
+class PathElement
+  attr_accessor :location, :parent
+  attr_reader :cost_to, :dist_from, :rating
+  def initialize(location=nil,parent=nil)
+    @location = location
+    @parent = parent
+    @cost_to = 0
+    @dist_from = 0
+    @rating = 99_999
+  end
+  
+  def cost_to=(new_cost)
+    @cost_to = new_cost
+    reset_rating
+  end
+  
+  def dist_from=(new_dist_from)
+    @dist_from = new_dist_from
+    reset_rating
+  end
+  
+  def reset_rating
+    @rating = @cost_to + @dist_from
+  end
+  
+  def to_s
+    "#{@location} at cost of #{@cost_to} and rating of #{@rating}"
+  end
+end
---
+++ test/test_polaris.rb
@@ -0,0 +1,235 @@
+require 'helper'
+require 'ai/polaris'
+
+# FakeLocation exibits an x,y,cost location
+class FakeLocation
+  attr_accessor :x,:y
+  def initialize(x,y);@x=x;@y=y;end
+  def ==(other)
+    @x == other.x and @y == other.y
+  end
+  def to_s
+    "#{@x},#{@y}"
+  end
+end
+
+# FakeMap exibits the contract that the map requires. 
+# Works on an X,Y grid that uses Ftors for 2D vectors
+class FakeMap
+  attr_accessor :w, :h
+  TRAVEL_COST_DIAG = 14
+  TRAVEL_COST_STRAIGHT = 10
+  
+  def initialize(w,h)
+    @w = w
+    @h = h
+    @grid = {}
+  end
+  
+  def place(location, thing)
+    @grid[location.x] ||= {}
+    @grid[location.x][location.y] = thing
+  end
+  
+  # is the location available for the specified type
+  def blocked?(location, type=nil)
+    return true if location.x >= @w || location.y >= @h || location.x < 0 || location.y < 0
+    if @grid[location.x] and @grid[location.x][location.y]
+      return true
+    else
+      return false
+    end
+  end
+  
+  # returns a list of neighbors and their distance
+  def neighbors(location)
+    x = location.x
+    y = location.y
+    [
+      FakeLocation.new(x-1, y-1),
+      FakeLocation.new(x-1, y+1),
+      FakeLocation.new(x+1, y-1),
+      FakeLocation.new(x+1, y+1),
+      FakeLocation.new(x-1, y),
+      FakeLocation.new(x+1, y),
+      FakeLocation.new(x, y-1),
+      FakeLocation.new(x, y+1)
+    ]
+  end
+  
+  def distance(from,to)
+    h_diagonal = [(from.x-to.x).abs, (from.y-to.y).abs].min
+    h_straight = ((from.x-to.x).abs + (from.y-to.y).abs)
+    return TRAVEL_COST_DIAG * h_diagonal + TRAVEL_COST_STRAIGHT * (h_straight - 2*h_diagonal)
+  end
+  
+  # return the cost to go from => to (assuming from and to are neighbors)
+  def cost(from, to)
+    if from.x == to.x or from.y == to.y
+      if from.x == to.x and from.y == to.y
+        0
+      else
+        TRAVEL_COST_STRAIGHT
+      end
+    else
+      TRAVEL_COST_DIAG
+    end
+  end
+end
+
+describe 'A new polaris' do
+  before do
+    @map = FakeMap.new 10, 20
+    @pather = Polaris.new @map
+  end
+
+  it 'should return an empty path if destination is not valid' do
+    from = FakeLocation.new @map.w-1, @map.h-1
+    to = FakeLocation.new @map.w, @map.h
+    @pather.guide(from,to).should == nil
+
+    to = FakeLocation.new(-1, -1)
+    @pather.guide(from,to).should == nil
+  end
+
+  it 'should return an empty path if start is not valid' do
+    from = FakeLocation.new @map.w, @map.h
+    to = FakeLocation.new @map.w-1, @map.h-1
+    @pather.guide(from,to).should == nil
+    
+    from = FakeLocation.new -1, -1
+    @pather.guide(from,to).should == nil
+  end
+  
+  it 'should return the path of "to" for accessible neighbor' do
+    from = FakeLocation.new 0, 0
+    to = FakeLocation.new 1, 0
+    
+    path = @pather.guide(from,to)
+    path.should != nil
+    path.size.should == 1
+    
+    path.first.cost_to.should == FakeMap::TRAVEL_COST_STRAIGHT
+    path.first.dist_from.should == 0
+    path.first.location.x.should == to.x
+    path.first.location.y.should == to.y
+  end
+  
+  it 'should return the right horizontal path of length 2' do
+    from = FakeLocation.new 0, 0
+    to = FakeLocation.new 2, 0
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 2
+    
+    path.first.location.x.should == 1
+    path.first.location.y.should == 0
+    path.last.location.should == to
+  end
+
+  it 'should return the left horizontal path of length 2' do
+    from = FakeLocation.new 2, 0
+    to = FakeLocation.new 0, 0
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 2
+    
+    path.first.location.x.should == 1
+    path.first.location.y.should == 0
+    path.last.location.should == to
+  end  
+  
+
+  it 'should return the left up diag path of length 2' do
+    from = FakeLocation.new 2, 2
+    to = FakeLocation.new 0, 0
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 2
+    
+    path.first.location.x.should == 1
+    path.first.location.y.should == 1
+    path.last.location.should == to
+  end  
+  
+  it 'should return the right up diag path of length 2' do
+    from = FakeLocation.new 2, 2
+    to = FakeLocation.new 4, 0
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 2
+    
+    path.first.location.x.should == 3
+    path.first.location.y.should == 1
+    path.last.location.should == to
+  end
+  
+  it 'should return the right down diag path of length 2' do
+    from = FakeLocation.new 2, 2
+    to = FakeLocation.new 4, 4
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 2
+    
+    path.first.location.x.should == 3
+    path.first.location.y.should == 3
+    path.last.location.should == to
+  end   
+  
+  it 'should return the left down diag path of length 2' do
+    from = FakeLocation.new 2, 2
+    to = FakeLocation.new 0, 4
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 2
+    
+    path.first.location.x.should == 1
+    path.first.location.y.should == 3
+    path.last.location.should == to
+  end  
+  
+  it 'should return go around an obstacle' do
+    from = FakeLocation.new 0, 0
+    to = FakeLocation.new 2, 0
+    
+    in_the_way = FakeLocation.new 1, 0
+    @map.place in_the_way, "ME"
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 2
+    
+    path.first.location.x.should == 1
+    path.first.location.y.should == 1
+    path.last.location.should == to
+  end
+  
+  it 'should return shortest path of horizontal and diag length 5' do
+    from = FakeLocation.new 0, 0
+    to = FakeLocation.new 5, 4
+    
+    path = @pather.guide(from,to)
+    
+    path.should != nil
+    path.size.should == 5
+    
+    # TODO how to check the shortest path?
+    # check each node to make sure they are neighbors?
+    path.first.location.x.should == 1
+    path.first.location.y.should == 1
+    path.last.location.should == to
+  end
+end
