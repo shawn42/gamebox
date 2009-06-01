daab8998fa05b7f7d498f5ad8705ac03e396dac3
Shawn Anderson <shawn42@gmail.com>
2009-05-31 22:37:59 -0700

    changed irb context to be in the game app

--- examples/nario/script/console.rb
+++ examples/nario/script/console.rb
@@ -1,9 +1,38 @@
 require '../config/environment'
+require 'irb'
+require 'console_app'
 
-libs = []
-libs << ' -r console_app'
+require 'irb'
+require 'irb/completion'
+module IRB
+  def IRB.start_session(obj)
+    unless $irb
+      IRB.setup nil
+      ## maybe set some opts here, as in parse_opts in irb/init.rb?
+      IRB.load_modules
+    end
+    
+    workspace = WorkSpace.new(obj)
 
-include_path = "-I " + $:.join(' -I ')
-cmd = "irb #{include_path} #{libs.join(' ')} --simple-prompt"
+    if @CONF[:SCRIPT] ## normally, set by parse_opts
+      $irb = Irb.new(workspace, @CONF[:SCRIPT])
+    else
+      $irb = Irb.new(workspace)
+    end
 
-exec cmd
+    @CONF[:IRB_RC].call($irb.context) if @CONF[:IRB_RC]
+    @CONF[:MAIN_CONTEXT] = $irb.context
+
+    trap("INT") do
+      $irb.signal_handle
+    end
+
+    catch(:IRB_EXIT) do
+      $irb.eval_input
+    end
+    print "\n"
+    
+    ## might want to reset your app's interrupt handler here
+  end
+end
+IRB.start_session @game
