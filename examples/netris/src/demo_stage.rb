4c95eff7e4fbd711ac24567017cd25e792baafcb
Shawn Anderson <shawn42@gmail.com>
2009-11-13 17:58:30 -0500

    got examples fixed w/ new stage stuff; unit tests still broken

--- examples/netris/src/demo_stage.rb
+++ examples/netris/src/demo_stage.rb
@@ -1,3 +1,15 @@
-require 'stage'
-class DemoStage < Stage
+require 'level'
+require 'ftor'
+class DemoLevel < Level
+  def setup
+    @game_field = create_actor :game_field
+    @game_field.x = 50
+    @game_field.y = 80
+  end
+
+  def draw(target,x_off,y_off)
+    target.fill [0,0,0,255]
+    super
+  end
 end
+
