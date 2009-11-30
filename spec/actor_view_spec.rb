bbe7adab3aa0da1c4b50011e2c9c8053b0822ee9
Shawn Anderson <shawn42@gmail.com>
2009-11-30 12:55:06 -0500

    specing out actor view

---
+++ spec/actor_view_spec.rb
@@ -0,0 +1,16 @@
+require File.join(File.dirname(__FILE__),'helper')
+require 'actor_view'
+
+describe 'A new actor view' do
+
+  it 'should be layered 0/1 by default' do
+    @test_me = ActorView.new :stage, Actor.new({})
+    @test_me.layer.should == 0
+    @test_me.parallax.should == 1
+  end
+
+  it 'should call setup on creation; tests that require subclassing seem wrong...'
+  it 'should accept layered behavior params from actor'
+  it 'should register for show/hide/remove events'
+  it 'should manage a cached surface for drawing'
+end
