69011f279a57930a142dee9d35869f176e2112a0
Jason Roelofs <jameskilton@gmail.com>
2010-01-21 19:39:01 -0500

    Remove debug

--- lib/gamebox/views/graphical_actor_view.rb
+++ lib/gamebox/views/graphical_actor_view.rb
@@ -14,18 +14,6 @@ class GraphicalActorView < ActorView
       x = x-w/2
       y = y-h/2
       img.blit target.screen, [x+x_off,y+y_off]
-      
-      if @actor.is? :physical and debug
-        @actor.physical.shapes.each do |shape|
-          bb = shape.bb
-          x = bb.l + x_off
-          y = bb.b + y_off
-          x2 = bb.r + x_off
-          y2 = bb.t + y_off
-          target.draw_box_s [x,y], [x2,y2], [255,25,25,150]
-        end
-      end
-      
     else
       if @actor.is?(:graphical) && @actor.graphical.tiled?
         x_tiles = @actor.graphical.num_x_tiles
