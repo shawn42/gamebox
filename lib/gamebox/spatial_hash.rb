2b6eb6363066ec0ae5e5b6a27480a04e2a3bcb12
Shawn Anderson <shawn42@gmail.com>
2010-04-05 15:27:35 -0400

    added spatial hash

---
+++ lib/gamebox/spatial_hash.rb
@@ -0,0 +1,72 @@
+class SpatialHash
+
+  attr_accessor :cell_size
+
+  def initialize(cell_size)
+    @cell_size = cell_size.to_f
+    @buckets = {}
+  end
+
+  def add(item)
+    buckets = lookup item
+    buckets.each do |bucket|
+      x,y = *bucket
+      @buckets[x] ||= {}
+      @buckets[x][y] ||= []
+      target_bucket = @buckets[x][y]
+      target_bucket << item unless target_bucket.include? item
+    end
+  end
+
+  def lookup(item)
+    w = 1
+    h = 1
+    w = item.width if item.respond_to? :width
+    h = item.height if item.respond_to? :height
+    x = item.x
+    y = item.y
+    min_x, min_y = bucket_for x, y
+    if w == 1 && h == 1
+      max_x = min_x
+      max_y = min_y
+    else
+      max_x, max_y = bucket_for x+w-1, y+h-1
+    end
+
+    buckets = []
+    (max_x-min_x+1).times do |i|
+      (max_y-min_y+1).times do |j|
+        buckets << [min_x+i,min_y+j] 
+      end
+    end
+
+    buckets
+  end
+
+  def bucket_for(x,y)
+    bucket_x = (x/cell_size).floor
+    bucket_y = (y/cell_size).floor
+    return [bucket_x, bucket_y]
+  end
+
+  def remove(item)
+    buckets = lookup item
+    buckets.each do |bucket|
+      x,y = *bucket
+      return if @buckets[x].nil? || @buckets[x][y].nil?
+      @buckets[x][y].delete item
+    end
+  end
+  
+  def items_at(x,y)
+    bucket_x = (x/@cell_size).floor
+    bucket_y = (y/@cell_size).floor
+    if @buckets[bucket_x].nil? || @buckets[bucket_x][bucket_y].nil?
+      return []
+    else
+      @buckets[bucket_x][bucket_y]
+    end
+  end
+
+end
+
---
+++ spec/spatial_hash_spec.rb
@@ -0,0 +1,84 @@
+require 'helper'
+require 'spatial_hash'
+
+describe 'a new SpacialHash' do
+  before do
+    @hash = SpatialHash.new 10
+  end
+  it 'should be constructable' do
+    @hash.cell_size.should == 10
+  end
+
+  it 'can add a point' do
+    pt = Point.new 2, 3
+    @hash.add pt
+    @hash.instance_variable_get('@buckets')[0][0].first.should == pt
+  end
+
+  it 'can add a neg point' do
+    pt = Point.new -2, 3
+    @hash.add pt
+    @hash.instance_variable_get('@buckets')[-1][0].first.should == pt
+  end
+
+  it 'can add a square' do
+    box = Item.new 2, 3, 12, 13
+    @hash.add box
+
+    buckets = @hash.instance_variable_get('@buckets')
+    buckets[0][0].first.should == box
+    buckets[0][1].first.should == box
+    buckets[1][0].first.should == box
+    buckets[1][1].first.should == box
+  end
+
+  it 'can add a box' do
+    box = Item.new 3, 3, 12, 2
+    @hash.add box
+
+    buckets = @hash.instance_variable_get('@buckets')
+    buckets[0][0].first.should == box
+    buckets[1][0].first.should == box
+
+    buckets[1][1].should be_nil
+    buckets[0][1].should be_nil
+  end
+
+  it 'can remove points' do
+    pt = Point.new -2, 3
+    @hash.add pt
+    @hash.remove pt
+
+    @hash.instance_variable_get('@buckets')[-1][0].should be_empty
+  end
+
+  it 'can remove boxes' do
+    box = Item.new 2, 3, 12, 13
+    @hash.add box
+
+    @hash.remove box
+
+    @hash.instance_variable_get('@buckets')[0][0].should be_empty
+    @hash.instance_variable_get('@buckets')[0][1].should be_empty
+    @hash.instance_variable_get('@buckets')[1][0].should be_empty
+    @hash.instance_variable_get('@buckets')[1][1].should be_empty
+  end
+
+  it 'can lookup objects for an x,y location' do
+    box = Item.new 2, 3, 12, 13
+    @hash.add box
+
+    @hash.items_at(5, 7).include?(box).should be_true
+  end
+end
+
+class Point
+  attr_accessor :x,:y
+  def initialize(x,y);@x=x;@y=y;end
+end
+
+class Item < Point
+  def initialize(x,y,w=1,h=1);@x=x;@y=y;@w=w;@h=h;end
+  def width;@w;end
+  def height;@h;end
+end
