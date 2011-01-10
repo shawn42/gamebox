4414f4cdec3b1fbb4a3dcc2143faf32b055c721c
Shawn Anderson <shawn42@gmail.com>
2011-01-10 18:03:45 -0500

    more of an fps test for jlnr of collision detection

--- script/perf_spatial_hash.rb
+++ script/perf_spatial_hash.rb
@@ -36,63 +36,26 @@ class Shape
   def actor_type;6;end
 end
 
-def do_it(hash, num_times, obj_size)
-  arb = Arb.new hash
-  rands = [1, 3, 5, 7, 13, 17]
-  num_times.times do |i|
-    shape = Shape.new(i+rands[-(i%6)], i+rands[-(i%6)],obj_size,obj_size,:circle,obj_size)
-    arb.register_collidable shape
-  end
+actor_count = 100
+loop_count = 100
+obj_size = 40
+cell_size = 100
 
-  hash.rehash
-
-  arb.find_collisions
+hash = SpatialHash.new cell_size#, true
+arb = Arb.new hash
+actor_count.times do |i|
+  shape = Shape.new(i*20, i*20, obj_size,obj_size,:circle,obj_size)
+  arb.register_collidable shape
 end
 
 Benchmark.bm(60) do|b|
-
-  @lots = 5000
-  @small_grid = 5
-  @medium_grid = 80
-
-  @small_object = 2
-  @medium_object = 20
-  @large_object = 100
-
-  puts "DEFS: "
-  puts "lots: #{@lots}"
-  puts "small_grid: #{@small_grid} medium_grid: #{@medium_grid} large_grid: #{@large_grid}"
-  puts "small_object: #{@small_object} medium_object: #{@medium_object} large_object: #{@large_object}"
-
-
-  klass = SpatialHash
-  # b.report("w/ lots of small objects in small size grid") do
-  #   hash = klass.new @small_grid, true
-  #   do_it(hash,@lots, @small_object)
-  # end
-
-  # b.report("w/ lots of medium objects in small size grid") do
-  #   hash = klass.new @small_grid, true
-  #   do_it(hash,@lots, @medium_object)
-  # end
-
-  b.report("w/ lots of small objects in medium size grid") do
-    hash = klass.new @medium_grid, true
-    do_it(hash,@lots, @small_object)
+  b.report("#{actor_count} actors w/ size #{obj_size} for #{loop_count} update loops") do
+    loop_count.times do
+      hash.rehash
+      arb.find_collisions
+    end
   end
-# 
-#   b.report("w/ lots of medium objects in medium size grid") do
-#     hash = klass.new @medium_grid, true
-#     do_it(hash,@lots, @medium_object)
-#   end
-# 
-#   b.report("w/ lots of large objects in small size grid") do
-#     hash = klass.new @small_grid, true
-#     do_it(hash,@lots, @large_object)
-#   end
-#   b.report("w/ lots of large objects in small size grid no resize") do
-#     hash = klass.new @small_grid, false
-#     do_it(hash,@lots, @large_object)
-#   end
+
 end
 
+puts "hash auto-size:#{hash.cell_size}"
