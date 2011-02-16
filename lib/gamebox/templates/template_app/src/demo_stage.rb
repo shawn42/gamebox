289d3c7d86d4b947fd7f149e68c9da8bc8bf6afb
Shawn Anderson <shawn42@gmail.com>
2011-02-15 21:40:38 -0500

    updating template to use spawn instead of create_actor

--- lib/gamebox/templates/template_app/src/demo_stage.rb
+++ lib/gamebox/templates/template_app/src/demo_stage.rb
@@ -1,7 +1,7 @@
 class DemoStage < Stage
   def setup
     super
-    @my_actor = create_actor :my_actor, :x => 10, :y => 10
+    @my_actor = spawn :my_actor, :x => 10, :y => 10
   end
 
   def draw(target)
