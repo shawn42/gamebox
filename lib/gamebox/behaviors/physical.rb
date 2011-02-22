1ed860138e99a9d440c963aef0860acf028a8433
Jason Porritt <jason.porritt@atomicobject.com>
2011-02-21 22:18:44 -0500

    Added the remaining constraint types and tests to the physical behavior.

--- lib/gamebox/behaviors/physical.rb
+++ lib/gamebox/behaviors/physical.rb
@@ -42,8 +42,10 @@ class Physical < Behavior
 
     # write code here to keep physics and x,y of actor in sync
     relegates :x, :y, :x=, :y=, :shape, :body, :parts,
-      :rotation, :warp, :segment_groups, :physical, 
-      :pivot, :spring
+      :rotation, :warp, :segment_groups, :physical,
+      :pivot, :pin, :spring, :slide, :groove,
+      :rotary_spring, :rotary_limit, :ratchet,
+      :gear, :motor
   end
 
   def setup_friction
@@ -179,12 +181,72 @@ class Physical < Behavior
     pivot
   end
 
+  def pin(my_anchor, other_physical, other_anchor)
+    pin = CP::Constraint::PinJoint.new(physical.body, other_physical.physical.body, my_anchor, other_anchor)
+    @actor.stage.register_physical_constraint pin
+    @constraints << pin
+    pin
+  end
+
   def spring(my_anchor, other_physical, other_anchor, rest_length, stiffness, damping)
     spring = CP::Constraint::DampedSpring.new(physical.body,other_physical.physical.body,
                                          my_anchor,other_anchor, rest_length, stiffness, damping)
     @actor.stage.register_physical_constraint spring
     @constraints << spring
     spring
+  end
+
+  def slide(my_anchor, other_physical, other_anchor, min_distance, max_distance)
+    slide = CP::Constraint::SlideJoint.new(physical.body, other_physical, my_anchor, other_anchor, min_distance, max_distance)
+    @actor.stage.register_physical_constraint slide
+    @constraints << slide
+    slide
+  end
+
+  def groove(groove_start, groove_end, other_physical, other_anchor)
+    groove = CP::Constraint::GrooveJoint.new(physical.body, other_physical, groove_start, groove_end, other_anchor)
+    @actor.stage.register_physical_constraint groove
+    @constraints << groove
+    groove
+  end
+
+  #
+  # All of these rotary joint types expect angles in radians.
+  #
+
+  def rotary_spring(other_physical, rest_angle, stiffness, damping)
+    rotary_spring = CP::Constraint::DampedRotarySpring.new(physical.body, other_physical, rest_angle, stiffness, damping)
+    @actor.stage.register_physical_constraint rotary_spring
+    @constraints << rotary_spring
+    rotary_spring
+  end
+
+  def rotary_limit(other_physical, min_angle, max_angle)
+    rotary_limit = CP::Constraint::RotaryLimitJoint.new(physical.body, other_physical, min_angle, max_angle)
+    @actor.stage.register_physical_constraint rotary_limit
+    @constraints << rotary_limit
+    rotary_limit
+  end
+
+  def ratchet(other_physical, phase, ratchet)
+    ratchet_joint = CP::Constraint::RatchetJoint.new(physical.body, other_physical, phase, ratchet)
+    @actor.stage.register_physical_constraint ratchet_joint
+    @constraints << ratchet_joint
+    ratchet_joint
+  end
+
+  def gear(other_physical, phase, ratio)
+    gear = CP::Constraint::GearJoint.new(physical.body, other_physical, phase, ratio)
+    @actor.stage.register_physical_constraint gear
+    @constraints << gear
+    gear
+  end
+
+  def motor(other_physical, rate)
+    motor = CP::Constraint::SimpleMotor.new(physical.body, other_physical, rate)
+    @actor.stage.register_physical_constraint motor
+    @constraints << motor
+    motor
   end
 
   def cleanup_constraints
--- spec/physical_spec.rb
+++ spec/physical_spec.rb
@@ -32,6 +32,18 @@ describe 'A new physical behavior' do
     end
   end
 
+  describe "#pin" do
+    it 'creates a new PinJoint and adds it to the space' do
+      other = CircleActor.new @opts
+      v1 = vec2(2,3)
+      v2 = vec2(4,5)
+      CP::Constraint::PinJoint.expects(:new).with(@physical.body, other.body, v1, v2).returns(:joint)
+      @stage.expects(:register_physical_constraint).with(:joint)
+
+      @actor.pin(v1, other, v2).should == :joint
+    end
+  end
+
   describe "#spring" do
     it 'creates a new DampedSpring and adds it to the space' do
       other = CircleActor.new @opts
@@ -41,6 +53,76 @@ describe 'A new physical behavior' do
       @stage.expects(:register_physical_constraint).with(:spring)
 
       @actor.spring(v1, other, v2, 1, 2, 3).should == :spring
+    end
+  end
+
+  describe "#slide" do
+    it 'creates a new SlideJoint and adds it to the space' do
+      other = CircleActor.new @opts
+      v1 = vec2(2,3)
+      v2 = vec2(4,5)
+      CP::Constraint::SlideJoint.expects(:new).with(@physical.body, other.body, v1, v2, 2,4).returns(:slide)
+      @stage.expects(:register_physical_constraint).with(:slide)
+
+      @actor.slide(v1, other.body, v2, 2, 4).should == :slide
+    end
+  end
+
+  describe "#groove" do
+    it 'creates a new GrooveJoint and adds it to the space' do
+      other = CircleActor.new @opts
+      v1 = vec2(2,3)
+      v2 = vec2(4,5)
+      v3 = vec2(9,7)
+      CP::Constraint::GrooveJoint.expects(:new).with(@physical.body, other.body, v1, v2, v3).returns(:groove)
+      @stage.expects(:register_physical_constraint).with(:groove)
+
+      @actor.groove(v1, v2, other.body, v3).should == :groove
+    end
+  end
+
+  describe "#rotary_spring" do
+    it 'creates a new DampedRotarySpring and adds it to the space' do
+      other = CircleActor.new @opts
+      CP::Constraint::DampedRotarySpring.expects(:new).with(@physical.body, other.body, 3.14, 10, 50).returns(:rotary_spring)
+      @stage.expects(:register_physical_constraint).with(:rotary_spring)
+      @actor.rotary_spring(other.body, 3.14, 10, 50).should == :rotary_spring
+    end
+  end
+
+  describe "#rotary_limit" do
+    it 'creates a new RotaryLimitJoint and adds it to the space' do
+      other = CircleActor.new @opts
+      CP::Constraint::RotaryLimitJoint.expects(:new).with(@physical.body, other.body, 0, 3).returns(:rotary_limit)
+      @stage.expects(:register_physical_constraint).with(:rotary_limit)
+      @actor.rotary_limit(other.body, 0, 3).should == :rotary_limit
+    end
+  end
+
+  describe "#ratchet" do
+    it 'creates a new RatchetJoint and adds it to the space' do
+      other = CircleActor.new @opts
+      CP::Constraint::RatchetJoint.expects(:new).with(@physical.body, other.body, 0, 0.3).returns(:ratchet)
+      @stage.expects(:register_physical_constraint).with(:ratchet)
+      @actor.ratchet(other.body, 0, 0.3).should == :ratchet
+    end
+  end
+
+  describe "#gear" do
+    it 'creates a new GearJoint and adds it to the space' do
+      other = CircleActor.new @opts
+      CP::Constraint::GearJoint.expects(:new).with(@physical.body, other.body, 0, 0.3).returns(:gear)
+      @stage.expects(:register_physical_constraint).with(:gear)
+      @actor.gear(other.body, 0, 0.3).should == :gear
+    end
+  end
+
+  describe "#motor" do
+    it 'creates a new SimpleMotor and adds it to the space' do
+      other = CircleActor.new @opts
+      CP::Constraint::SimpleMotor.expects(:new).with(@physical.body, other.body, 40).returns(:motor)
+      @stage.expects(:register_physical_constraint).with(:motor)
+      @actor.motor(other.body, 40).should == :motor
     end
   end
 
