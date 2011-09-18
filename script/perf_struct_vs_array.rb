c1ce379f28a44a9f914cec8c1bca078deb085cd3
Shawn Anderson <shawn42@gmail.com>
2011-09-18 15:37:37 -0400

    perf test array vs Point class

---
+++ script/perf_struct_vs_array.rb
@@ -0,0 +1,32 @@
+require 'benchmark'
+
+# Point = Struct.new :x, :y
+class Point
+  attr_accessor :x, :y
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
+end
+NUM = 10_000_000
+Benchmark.bm(60) do |b|
+  b.report("array") do
+    NUM.times do 
+      it = [4,6]
+      it[0] = 1
+      it[1] = 3
+      it[0]
+      it[1]
+    end
+  end
+  b.report("struct") do
+    NUM.times do 
+      it = Point.new 4, 6
+      it.x = 1
+      it.y = 3
+      it.x 
+      it.y 
+    end
+  end
+end
+
