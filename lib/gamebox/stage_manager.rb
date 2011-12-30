3022145f79efcaee6f84dd939e8fa8ac6f633ff0
Shawn Anderson <shawn42@gmail.com>
2011-12-29 19:11:00 -0500

    more specs for stage manager

--- lib/gamebox/stage_manager.rb
+++ lib/gamebox/stage_manager.rb
@@ -3,6 +3,8 @@ class StageManager
   constructor :resource_manager, :actor_factory, :input_manager,
     :sound_manager, :config_manager
 
+  attr_reader :backstage, :stage_names, :stage_opts
+
   def setup
     @stages = {}
     @backstage = Backstage.new
@@ -19,44 +21,6 @@ class StageManager
       opts ||= {}
       @stage_opts << opts
     end
-  end
-  
-  def lookup_stage_class(stage_name)
-    index = @stage_names.index stage_name
-    opts = @stage_opts[index]
-
-    name = opts[:class]
-    name ||= stage_name
-    stage_klass_name ||= Inflector.camelize name.to_s+"Stage"
-
-    begin
-      require name.to_s+"_stage"
-    rescue LoadError => ex
-      STDERR.puts ex
-      # TODO hrm.. should this get logged
-      # hope it's defined somewhere else
-    end
-    stage_klass = ObjectSpace.const_get stage_klass_name
-  end
-
-  def create_stage(name, opts)
-    stage_instance = lookup_stage_class(name).new(@input_manager, @actor_factory, @resource_manager, @sound_manager, @config_manager, @backstage, opts)
-
-    stage_instance.when :next_stage do |*args|
-      next_stage *args
-    end
-    stage_instance.when :prev_stage do |*args|
-      prev_stage *args
-    end
-    stage_instance.when :restart_stage do |*args|
-      restart_stage *args
-    end
-    stage_instance.when :change_stage do |stage_name, *args|
-      switch_to_stage stage_name, *args
-    end
-
-    
-    stage_instance
   end
 
   def next_stage(*args)
@@ -123,5 +87,35 @@ class StageManager
       @stage_args = nil
     end
   end
+
+  def lookup_stage_class(stage_name)
+    index = @stage_names.index stage_name
+    opts = @stage_opts[index]
+
+    name = opts[:class]
+    name ||= stage_name
+    stage_klass_name ||= Inflector.camelize name.to_s+"Stage"
+    stage_klass = ObjectSpace.const_get stage_klass_name
+  end
+
+  def create_stage(name, opts)
+    stage_instance = lookup_stage_class(name).new(@input_manager, @actor_factory, @resource_manager, @sound_manager, @config_manager, @backstage, opts)
+
+    stage_instance.when :next_stage do |*args|
+      next_stage *args
+    end
+    stage_instance.when :prev_stage do |*args|
+      prev_stage *args
+    end
+    stage_instance.when :restart_stage do |*args|
+      restart_stage *args
+    end
+    stage_instance.when :change_stage do |stage_name, *args|
+      switch_to_stage stage_name, *args
+    end
+
+    stage_instance
+  end
+
 
 end
--- spec/stage_manager_spec.rb
+++ spec/stage_manager_spec.rb
@@ -1,15 +1,80 @@
 require File.join(File.dirname(__FILE__),'helper')
 
-describe 'A new stage manager' do
+describe StageManager do
+  subject { StageManager.new resource_manager: :resource_manager,
+    config_manager: config_manager,
+    actor_factory: actor_factory,
+    input_manager: input_manager,
+    sound_manager: :sound_manager
+  }
+  let(:actor_factory) { stub('actor_factory', :stage_manager= => nil) }
+  let(:input_manager) { stub('input_manager', clear_hooks: nil) }
+  let(:config_manager) { stub('config_manager', load_config: stage_config) }
+  let(:stage_config) { {stages: [foo_stage_config]} }
+  let(:foo_stage_config) { {foo: {thing:1} } }
+  let(:bar_stage_config) { {bar: {thing:2} } }
   before do
-#    opts = {:resource_manager => stub, :actor_factory => stub, :input_manager => stub, 
-#      :sound_manager => stub, :config_manager => stub}
-#    @stage_manager = StageManager.new opts
+    Backstage.stubs(:new).returns :backstage
   end
 
-  it 'should construct' #do
-#    @stage_manager.should_not be_nil
-#  end
+  describe '#setup' do
+    it 'constructs' do
+      subject.should be
+    end
+
+    it 'setups the backstage' do
+      subject.backstage.should == :backstage
+    end
+
+    it 'installs its self on the actor factor' do
+      actor_factory.expects(:stage_manager=).with(instance_of(StageManager))
+      subject
+    end
+
+    it 'sets up the stage config' do
+      subject.stage_names.should == [:foo]
+      subject.stage_opts.should == [{thing:1}]
+    end
+  end
+
+  describe "#switch_to_stage" do
+    class FooStage; end
+    class BarStage; end
+    let(:foo_stage) { stub('foo stage', when: nil, curtain_raising: nil, curtain_dropping: nil) }
+    let(:bar_stage) { stub('bar stage', when: nil, curtain_raising: nil, curtain_dropping: nil) }
+    let(:stage_config) { {stages: [foo_stage_config, bar_stage_config]} }
+    before do
+      FooStage.stubs(:new).with(input_manager, actor_factory, 
+                                  :resource_manager, :sound_manager, 
+                                  config_manager, :backstage, {thing:1}).
+                                  returns(foo_stage)
+      BarStage.stubs(:new).returns(bar_stage)
+    end
+
+    it 'activates the new stage' do
+      subject.switch_to_stage :foo, :args
+      subject.current_stage.should == foo_stage
+    end
+
+    it 'raises the curtain on the new stage' do
+      foo_stage.expects(:curtain_raising).with(:args)
+      subject.switch_to_stage :foo, :args
+    end
+
+    it 'shutdowns the current stage' do
+      foo_stage.expects(:curtain_dropping).with(:other_args)
+      input_manager.expects(:clear_hooks).with(foo_stage)
+      subject.switch_to_stage :foo, :args
+      subject.switch_to_stage :bar, :other_args
+    end
+  end
+
+  describe 'callbacks' do
+    it 'registers next_stage'
+    it 'registers prev_stage'
+    it 'registers restart_stage'
+    it 'registers change_stage'
+  end
 
   describe '#prev_stage' do
     it 'should exit on prev_stage of first stage'
