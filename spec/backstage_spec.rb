de27c577862a0ef90d2dbe3c19d0a30dcc8fe7b1
Shawn Anderson <shawn42@gmail.com>
2012-04-01 23:19:38 -0400

    todo update; fixed backstage actor creation

--- spec/backstage_spec.rb
+++ spec/backstage_spec.rb
@@ -1,43 +1,38 @@
 require File.join(File.dirname(__FILE__),'helper')
 
 
-describe 'A new backstage' do
+describe Backstage do
 
-  before do
-    @backstage = Backstage.new
-  end
 
   it 'should construct' do
-    @backstage.should_not be_nil
+    subject.should_not be_nil
   end
 
   it 'should return nil if not found' do
-    @backstage.get(:foo).should be_nil
+    subject.get(:foo).should be_nil
   end
 
   it 'should return the value if found' do
-    @backstage.set(:foo,:foo_val).should == :foo_val
-    @backstage.get(:foo).should == :foo_val
+    subject.set(:foo,:foo_val).should == :foo_val
+    subject.get(:foo).should == :foo_val
   end
 
   it 'should replace with new values' do
-    @backstage.set(:foo,:foo_val).should == :foo_val
-    @backstage.get(:foo).should == :foo_val
+    subject.set(:foo,:foo_val).should == :foo_val
+    subject.get(:foo).should == :foo_val
 
-    @backstage.set(:foo,:other_val).should == :other_val
-    @backstage.get(:foo).should == :other_val
+    subject.set(:foo,:other_val).should == :other_val
+    subject.get(:foo).should == :other_val
   end
 
   it 'should properly offer bracket notation' do
-    @backstage[:foo] = :val
-    @backstage[:foo].should == :val
+    subject[:foo] = :val
+    subject[:foo].should == :val
   end
 
   it 'should raise if an Actor strolls off backstage' do
-    class Foo < Actor; end
-    foo = create_actor :foo
-
-    lambda{ @backstage[:foo] = foo }.should raise_error
+    foo = create_actor
+    lambda{ subject[:foo] = foo }.should raise_error
   end
 
 end
