6f54a076b333f76ed55a1bacad093b4e2f6611ad
Shawn Anderson <shawn42@gmail.com>
2012-04-18 00:11:37 -0400

    res man spec cleanup

--- spec/resource_manager_spec.rb
+++ spec/resource_manager_spec.rb
@@ -1,13 +1,12 @@
 require File.join(File.dirname(__FILE__),'helper')
 
 describe 'A new resource manager' do
-  before do
-    @res_man = ResourceManager.new :wrapped_screen => stub(:screen => :fake_gosu)
-  end
+  let(:actor) { stub 'actor', actor_type: 'string' }
+  subject { ResourceManager.new :wrapped_screen => stub(:screen => :fake_gosu) }
 
   it 'should load an actor image' do
-    @res_man.expects(:load_image).with("string.png").returns(:surf)
-    @res_man.load_actor_image("FoopyPants").should == :surf
+    subject.expects(:load_image).with("string.png").returns(:surf)
+    subject.load_actor_image(actor).should == :surf
   end
 
 end
