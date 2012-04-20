a61b3ecef4da05408eff674f0e25fd53e48e6174
Shawn Anderson <shawn42@gmail.com>
2012-04-19 23:44:08 -0400

    sketched out positioned behavior

---
+++ lib/gamebox/behaviors/positioned.rb
@@ -0,0 +1,13 @@
+Behavior.define :positioned do
+  requires :director
+  configure do
+    actor.has_attributes x: 0, y: 0
+    director.when :update do |time|
+      actor.react_to :position_changed if @x_dirty || @y_dirty
+      @x_dirty = false
+      @y_dirty = false
+    end
+    actor.when :x_changed do @x_dirty = true end
+    actor.when :y_changed do @y_dirty = true end
+  end
+end
---
+++ spec/behaviors/positioned_spec.rb
@@ -0,0 +1,6 @@
+require 'helper'
+
+describe :positioned do
+  it 'defines x,y on actor'
+  it 'rolls up x and y changes to position_changed on update'
+end
