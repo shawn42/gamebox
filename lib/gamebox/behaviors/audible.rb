f4f7685f0dbb82b631d8034edc7a686e1f9cc428
Shawn Anderson <shawn42@gmail.com>
2012-04-23 22:38:12 -0400

    converted audible behavior to DSL format

--- lib/gamebox/behaviors/audible.rb
+++ lib/gamebox/behaviors/audible.rb
@@ -1,23 +1,23 @@
 
 
-class Audible < Behavior
+Behavior.define :audible do
   
-  def setup
-    @sound_manager = @actor.sound_manager
-
-    audible_obj = self
-    relegates :play_sound, :stop_sound
+  requires :sound_manager
+  setup do
+    reacts_with :play_sound, :stop_sound
   end
 
-  # Plays a sound via the SoundManager.  See SoundManager for
-  # details on how to "define" sounds.
-  def play_sound(*args)
-    @sound_manager.play_sound *args
-  end
+  helpers do
+    # Plays a sound via the SoundManager.  See SoundManager for
+    # details on how to "define" sounds.
+    def play_sound(*args)
+      sound_manager.play_sound *args
+    end
 
-  # Stops a sound via the SoundManager.
-  def stop_sound(*args)
-    @sound_manager.stop_sound *args
+    # Stops a sound via the SoundManager.
+    def stop_sound(*args)
+      sound_manager.stop_sound *args
+    end
   end
 
 end
