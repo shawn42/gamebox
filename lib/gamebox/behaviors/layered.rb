76a1f4c471f5ce9576861bc92f20afd852ce3703
Shawn Anderson <shawn42@gmail.com>
2012-04-23 22:46:51 -0400

    converted layered behavior to new DSL

--- lib/gamebox/behaviors/layered.rb
+++ lib/gamebox/behaviors/layered.rb
@@ -2,33 +2,20 @@
 # Keeps track of the layer that something is to be drawn on.
 # By default it sets everything to layer 0 and parallax layer of
 # 1.
-class Layered < Behavior
+Behavior.define :layered do
 
-  attr_accessor :layer, :parallax
-  def setup
+  setup do
     if @opts.is_a? Hash
-      @layer = @opts[:layer]
-      @parallax = @opts[:parallax]
+      layer = @opts[:layer]
+      parallax = @opts[:parallax]
     else
-      @layer = @opts
+      layer = @opts
     end
     
-    @layer ||= 0
-    @parallax ||= 1
+    layer ||= 0
+    parallax ||= 1
 
-    relegates :layer=, :layer, :parallax=, :parallax, 
-      :layered
+    actor.has_attributes layer: layer, parallax: parallax
   end
 
-  def layered
-    self
-  end
-
-  def parallax=(new_parallax)
-    @parallax = new_parallax
-  end
-
-  def layer=(new_layer)
-    @layer = new_layer
-  end
 end
