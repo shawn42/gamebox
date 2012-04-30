9af5f650864857223ac9e9d064a7fc03904aaea9
Shawn Anderson <shawn42@gmail.com>
2012-04-30 12:37:03 -0400

    spec shell for physics manager

---
+++ spec/core/physics_manager_spec.rb
@@ -0,0 +1,11 @@
+require 'helper'
+
+
+describe PhysicsManager do
+  it 'needs tests'
+
+  describe "#add_collision_func" do
+    it 'pulls actors out of the collision shapes and yields them to the block'
+    it 'accepts two arrays of types and builds chipmunk handlers accordingly (see arbiter)'
+  end
+end
