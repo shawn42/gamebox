81591da84ac0dce8dacf51736464b3ff621d4ce9
Shawn Anderson <shawn42@gmail.com>
2012-07-23 08:38:29 -0400

    working; possibly move this to behavior redefinition

---
+++ lib/gamebox/core/debug_helpers.rb
@@ -0,0 +1,35 @@
+module DebugHelpers
+  def actors(type=nil)
+    matching_actors = []
+    ObjectSpace.each_object do |obj|
+      if obj.class == Actor 
+        if type.nil? || obj.actor_type == type
+          matching_actors << obj 
+        end
+      end
+    end
+    log "found #{matching_actors.size} actors"
+    matching_actors
+  end
+
+  def current_stage
+    stage_manager.current_stage
+  end
+
+  def reload_behavior(behavior_name)
+    behavior = behavior_name.to_sym
+    actors.each do |act|
+      if act.has_behavior?(behavior)
+        behaviors = act.instance_variable_get('@behaviors')
+        beh_opts = behaviors[behavior].opts
+
+        factory = act.this_object_context[:behavior_factory]
+        act.remove_behavior behavior
+
+        load "behaviors/#{behavior}.rb"
+
+        factory.add_behavior(act, behavior, beh_opts)
+      end
+    end
+  end
+end
