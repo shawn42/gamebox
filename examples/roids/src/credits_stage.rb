904659cc4a6ae4f31bfcb518424d6a8702626a99
Shawn Anderson <shawn42@gmail.com>
2012-11-26 00:01:52 -0500

    credits -> intro transition in roids

--- examples/roids/src/credits_stage.rb
+++ examples/roids/src/credits_stage.rb
@@ -4,7 +4,7 @@ define_stage :credits do
       create_actor :icon, image: 'credits.png', x: viewport.width / 2, y: viewport.height / 2
       timer_manager.add_timer "exit", 3000 do
         input_manager.reg :down, KbSpace do
-          exit
+          fire :change_stage, :intro
         end
       end
     end
