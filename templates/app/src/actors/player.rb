a44d323f68a315f322091fc5598bc461e7451839
Shawn Anderson <shawn42@gmail.com>
2012-12-04 00:35:09 -0500

    updated template actors draw code

--- templates/app/src/actors/player.rb
+++ templates/app/src/actors/player.rb
@@ -2,7 +2,9 @@ define_actor :player do
   has_behavior :positioned
   view do
     draw do |target, x_off, y_off, z|
-      target.draw_box actor.x,actor.y, 20, 20, Color::RED, 1
+      x = actor.x + x_off
+      y = actor.y + y_off
+      target.draw_box x,y, x+20, y+20, Color::RED, 1
     end
   end
 end
