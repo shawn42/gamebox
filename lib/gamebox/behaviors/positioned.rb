cdd744e955eac0eb18db1c4d27d0489a154b58a3
Shawn Anderson <shawn42@gmail.com>
2013-04-27 12:11:16 -0400

    fixed initial value of positioned attribute

--- lib/gamebox/behaviors/positioned.rb
+++ lib/gamebox/behaviors/positioned.rb
@@ -2,7 +2,8 @@ define_behavior :positioned do
   setup do
     x = opts[:x] || 0
     y = opts[:y] || 0
-    actor.has_attributes x: x, y: y, position: vec2(x,y)
+    actor.has_attributes x: x, y: y
+    actor.has_attributes position: vec2(actor.x, actor.y)
     actor.when(:x_changed) { actor.position = vec2(actor.x, actor.y) }
     actor.when(:y_changed) { actor.position = vec2(actor.x, actor.y) }
   end
