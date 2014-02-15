5dc966d89da3b5e3983361e1399fb8dd02815168
Shawn Anderson <shawn42@gmail.com>
2014-02-15 15:48:18 -0500

    position behavior keeps x,y up to date

--- lib/gamebox/behaviors/positioned.rb
+++ lib/gamebox/behaviors/positioned.rb
@@ -6,6 +6,13 @@ define_behavior :positioned do
     actor.has_attributes position: vec2(actor.x, actor.y)
     actor.when(:x_changed) { actor.position = vec2(actor.x, actor.y) }
     actor.when(:y_changed) { actor.position = vec2(actor.x, actor.y) }
+
+    actor.when(:position_changed) do
+      actor.update_attributes(
+        x: actor.position.x,
+        y: actor.position.y
+      )
+    end
   end
 
 end
--- spec/behaviors/positioned_spec.rb
+++ spec/behaviors/positioned_spec.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 describe :positioned do
 
-  subject { subcontext[:behavior_factory].add_behavior actor, :positioned, opts }
+  subject! { subcontext[:behavior_factory].add_behavior actor, :positioned, opts }
   let(:director) { evented_stub(stub_everything('director')) }
   let(:subcontext) do 
     it = nil
@@ -19,14 +19,16 @@ describe :positioned do
 
   context "empty options" do
     it 'defines x,y on actor' do
-      subject
       actor.x.should == 0
       actor.y.should == 0
+    end
+
+    it 'defines position' do
+      actor.position.should == vec2(0,0)
     end
   end
 
   it 'keeps position up to date w/ x and y' do
-    subject
     called = 0
     actor.when(:position_changed) do
       called += 1
@@ -37,16 +39,25 @@ describe :positioned do
     actor.y = 8
 
     called.should == 2
-    actor.position.x.should == 99
-    actor.position.y.should == 8
+    actor.position.should == vec2(99,8)
   end
 
   context "options passed in" do
     let(:opts) { { x: 5, y: 8} }
     it 'defines x,y on actor' do
-      subject
       actor.x.should == 5
       actor.y.should == 8
     end
+
+    it 'defines position on actor' do
+      actor.position.should == vec2(5,8)
+    end
+
+  end
+
+  it "keeps x and y up to date if position changes" do
+    actor.position = vec2(1,2)
+    actor.x.should == 1
+    actor.y.should == 2
   end
 end
